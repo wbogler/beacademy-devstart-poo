08161fc280c26119830284404b627ba110c99b40
Willian Bogler Da Silva <willian.silva@pti.org.br>
2022-06-21 23:36:34 -0300

    criação de rotas url

--- Desafio/public/index.php
+++ Desafio/public/index.php
@@ -10,20 +10,23 @@ use App\controller\ErrorController;
 
 $url = explode('?', $_SERVER['REQUEST_URI'])[0];
 
+function createRoute(string $controllerName, string $methodName){
+    return [
+        'controller'=> $controllerName,
+        'method'=> $methodName,
+    ];
+}
+
 $routes = [
-    '/' => []
+    '/' => createRoute(Indexcontroller::class, 'indexAction'),
+    '/produtos' =>createRoute(ProductController::class, 'listAction'),
 ];
 
-if($url === '/'){
-    $chamada = new Indexcontroller();
-    $chamada -> indexAction();
-}elseif($url == '/login'){
-    $chamada = new Indexcontroller();
-    $chamada -> loginAction();
-}elseif($url == '/produtos'){
-    $produto = new ProductController;
-    $produto->listAction();
-}else{
-    $errorPag = new ErrorController();
-    $errorPag->notFountAction();
+if (false === isset($routes[$url])){
+    (new ErrorController())->notFountAction();
 }
+
+$controllerName = $routes[$url]['controller'];
+$methodName - $routes[$url]['method'];
+
+(new $controllerName())->$methodName();
